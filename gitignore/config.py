9ebb7fefc9a1e7906fa103738d052027e6238dc9
Vincent <vincentd@gmx.us>
2020-12-10 20:02:31 +0100

    up

---
+++ gitignore/config.py
@@ -0,0 +1,8 @@
+"""Configuration settings for console app using device flow authentication
+"""
+
+CLIENT_ID = ''
+
+AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+RESOURCE = 'https://graph.microsoft.com'
+API_VERSION = 'beta'
